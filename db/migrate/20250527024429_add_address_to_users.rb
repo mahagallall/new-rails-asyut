40eaa1c132a76199b47a7047b931c03970529225
mahagallall <mahagalal252@gmail.com>
2025-05-27 05:46:40 +0300

    make address not null

--- db/migrate/20250527024429_add_address_to_users.rb
+++ db/migrate/20250527024429_add_address_to_users.rb
@@ -1,5 +1,5 @@
-class AddAddressToUsers < ActiveRecord::Migration[8.0]
+class AddAddressToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :address, :string
+    add_column :users, :address, :string, null: false
   end
 end
