ca871a3a0ef1cedc5831e72f89fd2b454bc0d783
mahagallall <mahagalal252@gmail.com>
2025-05-27 22:41:24 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-user1 = User.create(name: "Maha", dob: "2000-08-22", email: "maha@example.com", phone_number: "01040943934")
-user2 = User.create(name: "Aya", dob: "2000-11-01", email: "aya@example.com", phone_number: "0111111111")
-post1 = Post.create(title: "First Post", content: "Content here", user_id: user1)
-post2 = Post.create(title: "Second Post", content: "More content", user_id: user2)
+user1 = User.create!(name: "Maha", dob: "2000-08-22", email: "maha@example.com", phone_number: "01040943934")
+user2 = User.create!(name: "Aya", dob: "2000-11-01", email: "aya@example.com", phone_number: "0111111111")
+post1 = Post.create!(title: "First Post", content: "Content here", user_id: user1.id)
+post2 = Post.create!(title: "Second Post", content: "More content", user_id: user2.id)
 
-EditorsPost.create(user: user2, post: post1) 
-EditorsPost.create(user: user1, post: post2)
+EditorsPost.create!(user_id: user2.id, post_id: post1.id)
+EditorsPost.create!(user_id: user1.id, post_id: post2.id)
