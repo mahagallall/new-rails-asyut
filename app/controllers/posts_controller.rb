24d29d0a476281549654c37c079f1fed7a234d23
mahagallall <mahagalal252@gmail.com>
2025-05-27 04:56:06 +0300

    add posts_actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,41 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(title: params[:title], content: params[:content])
+    if @post.save
+      redirect_to @post, notice: "Successfully created post."
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:title], content: params[:content])
+      redirect_to @post, notice: "Successfully updated post."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Successfully deleted post."
   end
 end
